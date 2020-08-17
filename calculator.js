252ce0e9cc062a7bf134a0feaf579b4d82c12183
ofentsesambo <ofentsesambo@gmail.com>
2020-08-17 21:42:24 +0200

    almost done

--- calculator.js
+++ calculator.js
@@ -22,7 +22,53 @@ function add(str){
         return sum;
     }
 
-    }      
+    function excapingSpecChar(regCharacters) {
+        return regCharacters.replace(/[*&$.=*+?\\(){}\/\[\]]/g, "\\$&")
+    }
+
+    if (str.startsWith('//')) {
+        let delimeter = str.split(/\/\/(.*)\n/g)[1],
+            numSec = str.split(/\/\/(.*)\n/g)[2];
+        delimeter = new RegExp(excapingSpecChar(delimeter), "g");
+        numSplit = numSec.replace(delimeter, ",").split(",")
+
+    }
+
+    if (str.startsWith('//[')) {
+        let delimeter = str.match(/(?<=\/\/).*(?=\n)/g)[0],
+            replacer = delimeter.replace("[", ""),
+            replacer2 = replacer.replace(/\]\[/g, "|"),
+            replacer3 = replacer2.replace("]", ""),
+            numSec = str.match(/(?<=\n).*/g)[0];
+
+        delimeter = new RegExp(excapingSpecChar(replacer3), "g");
+        numSplit = numSec.replace(delimeter, ",").split(",");
+    }
+
+    for (let i = 0; i < numSplit.length; i++) {
+        if (numSplit[i] >= 1000) {
+            continue;
+        }
+        sum += parseInt(numSplit[i])
+    }
+
+    for (let i = 0; i < numSplit.length; i++) {
+        if (numSplit[i] < 0) {
+            negatives.push(parseInt(numSplit[i]));
+        }
+    }
+    if (negatives.length > 0) {
+        throw ("negatives not allowed " + negatives);
+    }
+    if (str.match(/\D$/) || isNaN(sum)) {
+        throw Error("Invalid input")
+    }
+    return sum
+}
+
+module.exports ={ add }
+
+     
 
 console.log(add("//\n1;2"))
 
---
+++ spec/stringCalculator.spec.js
@@ -0,0 +1,75 @@
+let myAdd = require("../calculator");
+let add = myAdd.add
+
+describe("add", () => {
+  it(`should return 0`, () => {
+    expect(add("")).toBe(0);
+  });
+  it(`should return 1`, () => {
+    expect(add("1")).toBe(1);
+  });
+  it(`should return 2`, () => {
+    expect(add("1,1")).toBe(2);
+  });
+  it(`should return 10`, () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+  it(`should return 6`, () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+  it(`should return 3`, () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+  it(`should return 3`, () => {
+    expect(add("//4\n142")).toBe(3);
+  });
+  it(`should return  Negatives not allowed`, () => {
+    expect(function () {
+      add("-1,2,3,4")
+    }).toThrow("negatives not allowed -1")
+  });
+  it(`should return 3`, () => {
+    expect(add("//;\n1000;1;2")).toBe(3);
+  });
+  it(`should return 3`, () => {
+    expect(add("//***\n1***2***3")).toBe(6);
+  });
+  
+  it(`should throw error`, () => {
+    expect(add("-1,-2,3,4")).toThrow("negatives not allowed -1,-2");
+  });
+
+  it(`should return 3`, () => {
+    expect(add("//;\n1000;1;2")).toBe(3);
+  });
+
+  it(`should return 6`, () => {
+    expect(add("//***\n1***2***3")).toBe(6);
+  });
+
+  it(`should return 6`, () => {
+    expect(add("//[:D][%]\n1:D2%3")).toBe(6);
+  });
+  
+  it(`should return 6`, () => {
+    expect(add("[***][%%%]\n1***2%%%3")).toBe(6);
+  });
+  
+  it(`should return 6`, () => {
+    expect(add("//[(-_-')][%]\n1(-_-')2%3")).toBe(6);
+  });
+
+  it(`should throw error 2`, () => {
+    expect(add("//;\n1000;1;2;")).toThrow("Invalid input");
+  });
+
+  it(`should throw error 3`, () => {
+    expect(add("   //;\n1000,1;2")).toThrow("Invalid input");
+  });
+
+  it(`should throw error 4`, () => {
+    expect(add("1,2,3//;\n1000,1;2")).toThrow("Invalid input");
+  });
+  
+
+});
